594839d1b9a90e370d5b90746bd5559e77dfb180
Greta <96105703+thisIsGreta@users.noreply.github.com>
2022-04-10 11:35:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path = "/Users/username/Development/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
@@ -13,4 +14,11 @@ WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/bod
 number_of_articles = articles.get_attribute("textContent")
 print(number_of_articles)
 
-driver.quit()
+# driver.quit()
+
+fname = driver.find_element(By.NAME, "fName")
+fname.click()
+fname.send_keys("Greta")
+
+sign_up = driver.find_element(By.CLASS_NAME, "btn")
+sign_up.send_keys(Keys.ENTER)
